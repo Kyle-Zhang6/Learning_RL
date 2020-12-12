e7e18e30682e8f6e921e851725e19c0140602bd1
Kyle-Zhang6 <leochang6@outlook.com>
2020-12-12 16:20:30 +0800

    Update A2C.py

--- CartPole/A2C.py
+++ CartPole/A2C.py
@@ -13,13 +13,11 @@ class Model(nn.Module):
         super(Model, self).__init__()
         self.fc1 = nn.Linear(input_feature, hidden_feature)
         self.fc2 = nn.Linear(hidden_feature, output_feature)
-        self.fc3 = nn.Linear(hidden_feature, hidden_feature)
 
         self.optimizer = optim.Adam(self.parameters(), lr=lr)
 
     def forward(self, x):
         x_ = F.relu(self.fc1(x))
-        # x_ = F.relu(self.fc3(x_))
         x_ = self.fc2(x_)
         return x_
 
@@ -27,18 +25,15 @@ class Model(nn.Module):
 class Actor:
     def __init__(self, input_feature, hidden_feature, output_feature, lr=0.01):
         self.net = Model(input_feature, hidden_feature, output_feature, lr=lr)
-        self.net.to(DEVICE)
 
     def evaluate_action_prob(self, state, requires_grad=False):
-        if not isinstance(state, torch.Tensor):
-            state = torch.FloatTensor(state).unsqueeze(0).to(DEVICE)
         if requires_grad:
             self.net.train()
-            action_prob = F.softmax(self.net(state), dim=-1).squeeze(0)
+            action_prob = F.softmax(self.net(state), dim=-1)
         else:
             with torch.no_grad():
                 self.net.eval()
-                action_prob = F.softmax(self.net(state), dim=-1).squeeze(0)
+                action_prob = F.softmax(self.net(state), dim=-1)
         return action_prob
 
     def decide(self, state):
@@ -47,9 +42,11 @@ class Actor:
         action = p.sample().item()
         return action
 
-    def learn(self, discount, td_error, state, action):
-        p = self.evaluate_action_prob(state, requires_grad=True)[action]
-        loss = - discount * td_error * torch.log(p)
+    def learn(self, td_error, state, action):
+        p = self.evaluate_action_prob(state, requires_grad=True)
+        p = p.gather(1, action)
+        loss = - td_error * torch.log(p)
+        loss = loss.mean()
         self.net.optimizer.zero_grad()
         loss.backward()
         self.net.optimizer.step()
@@ -58,26 +55,52 @@ class Actor:
 class Critic:
     def __init__(self, input_feature, hidden_feature, output_feature=1, lr=0.01):
         self.net = Model(input_feature, hidden_feature, output_feature, lr=lr)
-        self.net.to(DEVICE)
 
     def evaluate_v(self, state, requires_grad=False):
-        if not isinstance(state, torch.Tensor):
-            state = torch.FloatTensor(state).unsqueeze(0).to(DEVICE)
         if requires_grad:
             self.net.train()
-            v = self.net(state).squeeze(0)
+            v = self.net(state)
         else:
             with torch.no_grad():
                 self.net.eval()
-                v = self.net(state).squeeze(0)
+                v = self.net(state)
         return v
 
     def learn(self, state, target_U):
         v = self.evaluate_v(state, requires_grad=True)
-        loss = F.smooth_l1_loss(v, target_U)
+        loss = F.mse_loss(v, target_U)
+        loss = loss.mean()
         self.net.optimizer.zero_grad()
         loss.backward()
         self.net.optimizer.step()
+
+
+class Memory:
+    def __init__(self):
+        self.trajectory = []
+
+    def push(self, step):
+        self.trajectory.append(step)
+
+    def clear(self):
+        self.trajectory = []
+
+    def make_batch(self):
+        s1_list, a_list, r_list, s2_list, done_list = [], [], [], [], []
+        for t in self.trajectory[::-1]:
+            s1, a, r, s2, done = t
+            s1_list.append(s1)
+            a_list.append([a])
+            r_list.append([r])
+            s2_list.append(s2)
+            done_list.append([done])
+
+        s1_list, s2_list = torch.FloatTensor(s1_list), torch.FloatTensor(s2_list)
+        r_list, done_list = torch.FloatTensor(r_list), torch.FloatTensor(done_list)
+        a_list = torch.tensor(a_list, dtype=torch.int64)
+        self.clear()
+
+        return s1_list, a_list, r_list, s2_list, done_list
 
 
 def revise_reward(reward, steps, done):
@@ -86,15 +109,14 @@ def revise_reward(reward, steps, done):
     return reward
 
 
-DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
 if __name__ == '__main__':
     ENV_NAME        = 'CartPole-v0'
-    TOTAL_EPISODE   = 2000000
-    RENDER_GAP      = 1000
-    GAMMA           = 0.95
-    ACTOR_LR        = 0.00005
-    CRITIC_LR       = 0.0001
+    TOTAL_EPISODE   = 10000
+    RENDER_GAP      = 100
+    GAMMA           = 0.99
+    ACTOR_LR        = 0.0003
+    CRITIC_LR       = 0.001
+    TRAJECTORY_L    = 10  # Length of trajectory for each training step
 
     env = gym.make(ENV_NAME)
     state_dim = env.observation_space.shape[0]
@@ -102,32 +124,38 @@ if __name__ == '__main__':
 
     actor = Actor(state_dim, 128, action_dim, lr=ACTOR_LR)
     critic = Critic(state_dim, 128, output_feature=1, lr=CRITIC_LR)  # Only output 1 score for the state
+    memory = Memory()
 
     episode_rewards = []
     for ep in range(TOTAL_EPISODE):
         render = True if ((ep % RENDER_GAP) == RENDER_GAP - 1) else False
         done = False
         episode_reward = 0
-        discount = 1.
 
-        state = env.reset()
+        s1 = env.reset()
         while not done:
-            if render:
-                env.render()
-            # Get all useful information
-            action = actor.decide(state)
-            state_next, reward, done, _ = env.step(action)
-            episode_reward += reward
-            reward = revise_reward(reward, episode_reward, done)
+            for i in range(TRAJECTORY_L):
+                if render:
+                    env.render()
+                # Get all useful information
+                a = actor.decide(torch.FloatTensor(s1))
+                s2, r, done, _ = env.step(a)
+                episode_reward += r
+                r = revise_reward(r, episode_reward, done)
+
+                memory.push((s1, a, r, s2, done))
+                s1 = s2
+                if done:
+                    break
+
+            state, action, reward, state_next, done_ = memory.make_batch()
             # Train both the actor and the critic
-            U = reward + GAMMA * critic.evaluate_v(state_next, requires_grad=False) * (1. - float(done))
+            U = reward + GAMMA * critic.evaluate_v(state_next, requires_grad=False) * (1. - done_)
             v = critic.evaluate_v(state, requires_grad=False)
             td_error = U - v
             p = actor.evaluate_action_prob(state,requires_grad=False)
-            actor.learn(discount, td_error.detach(), state, action)
+            actor.learn(td_error.detach(), state, action)
             critic.learn(state, U.detach())
-            # Update
-            discount *= GAMMA
 
         episode_rewards.append(episode_reward)
         print('Episode NO.{}: reward = {}'.format(ep+1, episode_reward))
--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 ## Learning RL
 
-##### As the record of my process in learning Reinforcement Learning
+##### As the record of my process in learning Reinforcement Learning.
+
+##### Using GYM (@OpenAI) as the interactive environment.
 
 - __CartPole__:
 
-  Applying algorithms like DQN / VPG / AC on GYM environment 'CartPole'
+  Applying all kinds of algorithms like DQN / VPG / AC on GYM environment 'CartPole'
+
